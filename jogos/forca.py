7b51506a5a64afb186ccf416ea020f5aab721ab0
Henrique Chagas <henrique.chagas@aluno.ufop.edu.br>
2022-02-07 09:11:03 -0300

    Capitulo 1 aula 05 Game Loop

--- jogos/forca.py
+++ jogos/forca.py
@@ -5,6 +5,16 @@ def jogar():
     print("**BEM VINDO AO JOGO DA FORCA!**")
     print("********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("JOGANDO!")
+
+
+
     print("FIM DO JOGO!!")
 
 if(__name__ == "__main__"):
