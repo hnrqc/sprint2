1ac8913712625181f6c2cae4260300d5aae3cdc8
Henrique Chagas <henrique.chagas@aluno.ufop.edu.br>
2022-02-09 09:30:44 -0300

    Modulo 1 aula 03 Dados e comportamento

--- Orientacao a Objetos/teste.py
+++ Orientacao a Objetos/teste.py
@@ -1,3 +1,12 @@
 def cria_conta(numero,titular,saldo,limite):
     conta = {"numero": numero, "titular": titular, "saldo":saldo, "limite":limite}
     return conta
+
+def deposita(conta, valor):
+    conta["sando"] += valor
+
+def saque(conta, valor):
+    conta["sando"] -= valor
+
+def extrato(conta):
+    print("Saldo é {}".format(conta["saldo"]))
