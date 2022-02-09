b6c90d4d05cb760d46e49bf7706b3fe3f5a2eacc
Henrique Chagas <henrique.chagas@aluno.ufop.edu.br>
2022-02-09 11:22:56 -0300

    Modulo 4 aula 01 Atributos privados

--- Orientacao a Objetos/conta.py
+++ Orientacao a Objetos/conta.py
@@ -2,13 +2,13 @@ class Conta:
 
     def __init__(self,numero,titular,saldo,limite):
         print("Construindo Objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo é {} do titular {}".format(self.saldo,self.titular))
+        print("Saldo é {} do titular {}".format(self.__saldo,self.__titular))
 
     def deposita(self,valor):
         self.saldo += valor
