7d8506582d0643a8f1bd3bdae6da6e0fe7a3fbc9
Henrique Chagas <henrique.chagas@aluno.ufop.edu.br>
2022-02-09 10:05:26 -0300

    Modulo 3 aula 01 Usando métodos

--- Orientacao a Objetos/conta.py
+++ Orientacao a Objetos/conta.py
@@ -6,3 +6,13 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("Saldo é {} do titular {}".format(self.saldo,self.titular))
+
+    def deposita(self,valor):
+        self.saldo += valor
+
+    def saque(self, valor):
+        self.saldo -= valor
+
