0140701f4d401c76549b9a7d74aea0e65aa631d1
Henrique Chagas <henrique.chagas@aluno.ufop.edu.br>
2022-02-11 10:23:10 -0300

    Modulo 6 aula 01 Métodos privados

--- Orientacao a Objetos/conta.py
+++ Orientacao a Objetos/conta.py
@@ -13,17 +13,27 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar (self, valor_a_sacar):
+        valor_disponivel_para_saque = self.__saldo+self.__limite
+        return valor_a_sacar <= valor_disponivel_para_saque
+
     def saque(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("Você Não tem saldo disponivel para esta operação, seu saldo atual é R${} e seu limite atual é R${}".format(self.__saldo,self.__limite))
+
 
     def transfere(self, valor, destino):
         self.saque(valor)
         destino.deposita(valor)
 
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
 
-    def get_titular(self):
+    @property
+    def titular(self):
         return self.__titular
 
     @property
